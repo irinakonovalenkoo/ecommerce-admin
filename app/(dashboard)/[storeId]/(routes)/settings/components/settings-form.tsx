d7289b2c4ef74a7f16624236a59eff6601e2e0fd
Irina <irinagorea182002@gmail.com>
2025-02-21 18:03:16 +0200

    Billboards Entity (Admin)

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -35,6 +35,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
 
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
