f1eaeaecfd2651bb4eada45886b9739d9ac79f64
Irina <irinagorea182002@gmail.com>
2025-02-23 22:04:39 +0200

    Data Table (Admin)

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,8 +5,18 @@ import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
-export const BillboardClient = () => {
+
+interface BillboardClienProps {
+    data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClienProps> = ({
+    data
+}) => {
     const router = useRouter();
     const params = useParams();
 
@@ -14,7 +24,7 @@ export const BillboardClient = () => {
     <>
         <div className="flex items-center justify-between">
             <Heading 
-                title={`Billboards (0)`}
+                title={`Billboards (${data.length})`}
                 description="Manage billboards for your store"/>
             <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                 <Plus className="mr-2 h-4 w-4"/>
@@ -22,6 +32,10 @@ export const BillboardClient = () => {
             </Button>
         </div>
         <Separator />
+        <DataTable searchKey="label" columns={columns} data={data}/>
+        <Heading title="API" description="API calls for Billboards"/>
+        <Separator />
+        <ApiList entityName="billboards" entityIdName="billboadId"/>
     </>
   )
 }
--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
-  '/sign-up(.*)'
+  '/api/:path*'
 ])
+
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
